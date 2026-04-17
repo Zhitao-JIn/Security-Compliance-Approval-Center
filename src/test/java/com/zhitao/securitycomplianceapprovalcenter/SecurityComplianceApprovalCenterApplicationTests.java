e14f7e27337f418e18e889750762d53233e581f8
Zhitao-Jin <zhitaojin2003@gmail.com>
2026-04-17 09:26:43 +0200

    init

---
+++ src/test/java/com/zhitao/securitycomplianceapprovalcenter/SecurityComplianceApprovalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhitao.securitycomplianceapprovalcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityComplianceApprovalCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
